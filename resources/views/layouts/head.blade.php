02e85814b5f7803e6cfa751e2b0fda4a43caa168
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-02-22 14:23:11 +0200

    Create this view that contain "Css" links

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,34 @@
+<!-- Title -->
+<title>@yield('title')</title>
+
+<!-- Icons css -->
+<link href="../../assets/css/icons.css" rel="stylesheet">
+
+<!-- Bootstrap css -->
+<link href="../../assets/plugins/bootstrap/css/bootstrap.rtl.min.css" rel="stylesheet">
+
+<!--  Owl-carousel css-->
+<link href="../../assets/plugins/owl-carousel/owl.carousel.css" rel="stylesheet" />
+
+<!-- P-scroll bar css-->
+<link href="../../assets/plugins/perfect-scrollbar/p-scrollbar.css" rel="stylesheet" />
+
+<!--  Right-sidemenu css -->
+<link href="../../assets/plugins/sidebar/sidebar.css" rel="stylesheet">
+
+<!-- Sidemenu css -->
+<link rel="stylesheet" href="../../assets/css-rtl/sidemenu.css">
+
+<!-- Maps css -->
+<link href="../../assets/plugins/jqvmap/jqvmap.min.css" rel="stylesheet">
+
+<!-- style css -->
+<link href="../../assets/css-rtl/style.css" rel="stylesheet">
+<link href="../../assets/css-rtl/style-dark.css" rel="stylesheet">
+<link href="../../assets/css-rtl/boxed.css" rel="stylesheet">
+<link href="../../assets/css-rtl/dark-boxed.css" rel="stylesheet">
+
+<!---Skinmodes css-->
+<link href="../../assets/css-rtl/skin-modes.css" rel="stylesheet" />
+
+@yield('css')
