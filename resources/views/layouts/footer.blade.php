ba01ff6b7600c860b50ecf17130292394f639580
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-02-22 14:25:31 +0200

    Create this view that contain the footer codes

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<!-- Footer opened -->
+<div class="main-footer ht-40">
+    <div class="container-fluid pd-t-0-f ht-100p">
+        <span>
+            Copyright © 2022 <a href="#">Outletship</a>. Designed by <a href="https://golden-soft.online/">
+                Golden Soft
+            </a>
+            All rights reserved.</span>
+    </div>
+</div>
+<!-- Footer closed -->
