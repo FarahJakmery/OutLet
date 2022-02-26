1f3bede2bda9b11645d51597620b14610e6c3059
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-02-26 12:31:08 +0200

    Implement "Index, Store, Update, Destroy" Methodes in BrandController

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -17,7 +17,8 @@ class BrandController extends Controller
      */
     public function index()
     {
-        //
+        $brands = Brand::all();
+        return view("Admin.Brands.brands", compact('brands'));
     }
 
     /**
@@ -38,7 +39,36 @@ class BrandController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate(
+            [
+                'brand_name'   => 'required|unique:brands|max:255',
+                'description'  => 'required|string|min:10|max:255',
+                'logo_name'    => 'required|mimes:jpeg,png,jpg'
+            ],
+            [
+                'brand_name.required'  => 'Please enter the Brand Name',
+                'brand_name.unique'    => 'Brand Name is already registered',
+                'description.required' => 'Please enter the Brand Description',
+                'logo_name.mimes'      => 'Error, Logo must be in one of the required formats'
+            ]
+        );
+
+        $image = $request->file('logo_name');
+        $file_name = $image->getClientOriginalName();
+
+        Brand::create([
+            'brand_name'  => $request->brand_name,
+            'description' => $request->description,
+            'logo_name'   => $file_name
+        ]);
+
+        // move logo
+        // اسم المرفق سيتم حفظه في الداتابيز و لكن المرفق بحد ذاته سيتم حفظه على السيرفر في المكان الذي سنقوم بتحديده
+        $imageName = $request->logo_name->getClientOriginalName();
+        $request->logo_name->move(public_path('BrandsLogos/' . $request->brand_name), $imageName);
+
+        session()->flash('Add', 'Brand added successfully');
+        return redirect('/brands');
     }
 
     /**
@@ -72,7 +102,39 @@ class BrandController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $id = $request->id;
+        $request->validate(
+            [
+                'brand_name'   => 'required|max:255|unique:brands,brand_name,' . $id,
+                'description'  => 'required|string|min:10|max:255',
+                'logo_name'    => 'required|mimes:jpeg,png,jpg'
+            ],
+            [
+                'brand_name.required'  => 'Please enter the Brand Name',
+                'brand_name.unique'    => 'Brand Name is already registered',
+                'description.required' => 'Please enter the Brand Description',
+                'logo_name.mimes'      => 'Error, Logo must be in one of the required formats'
+            ]
+        );
+
+        $image = $request->file('logo_name');
+        $file_name = $image->getClientOriginalName();
+
+        $brand = Brand::find($id);
+
+        $brand->update([
+            'brand_name'   => $request->brand_name,
+            'description'  => $request->description,
+            'logo_name'    => $file_name
+        ]);
+
+        // move logo
+        // اسم المرفق سيتم حفظه في الداتابيز و لكن المرفق بحد ذاته سيتم حفظه على السيرفر في المكان الذي سنقوم بتحديده
+        $imageName = $request->logo_name->getClientOriginalName();
+        $request->logo_name->move(public_path('BrandsLogos/' . $request->brand_name), $imageName);
+
+        session()->flash('edit', 'Brand has been successfully modified');
+        return redirect('/brands');
     }
 
     /**
@@ -83,6 +145,9 @@ class BrandController extends Controller
      */
     public function destroy(Request $request)
     {
-        //
+        $id = $request->id;
+        Brand::find($id)->delete();
+        session()->flash('delete', 'Brand has been removed successfully');
+        return redirect('/brands');
     }
 }
