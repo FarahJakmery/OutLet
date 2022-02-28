53528257fbcf7dcdcba8e69502a9346017dddb7a
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-02-28 08:01:22 +0200

    Create a Pivot table between "brand table" and "mcategories table"

---
+++ database/migrations/2022_02_27_104037_create_brand_mcatrgory_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBrandMcatrgoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brand_mcategory', function (Blueprint $table) {
+            $table->foreignId('brand_id')->constrained()->onDelete('cascade');;
+            $table->foreignId('mcategory_id')->constrained()->onDelete('cascade');;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brand_mcatrgory');
+    }
+}
