7bef31c2ef5471655cbbe438a647cc73685c0712
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-02-28 08:37:56 +0200

    Create a Pivot table between "mcategory table" and "subcategory"

---
+++ database/migrations/2022_02_28_062724_create_mcategory_subcategory_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMcategorySubcategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mcategory_subcategory', function (Blueprint $table) {
+            $table->foreignId('mcategory_id')->constrained()->onDelete('cascade');;
+            $table->foreignId('subcategory_id')->constrained()->onDelete('cascade');;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mcategory_subcategory');
+    }
+}
