ef6a7313de837206657c767661b8ea07f9bab151
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-03-03 14:28:39 +0200

    Edit "index,store,update,destroy" Methodes in McategoryController

--- app/Http/Controllers/Admin/McategoryController.php
+++ app/Http/Controllers/Admin/McategoryController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Brand;
 use App\Models\Mcategory;
-use Illuminate\Http\Request;
 
 class McategoryController extends Controller
 {
@@ -16,8 +17,8 @@ class McategoryController extends Controller
      */
     public function index()
     {
-        $MCtegories = Mcategory::all();
-        $brands = Brand::all();
+        $MCtegories = Mcategory::translated()->get();
+        $brands = Brand::translated()->get();
         return view('Admin.MainCategory.mainCategories', compact('MCtegories', 'brands'));
     }
 
@@ -39,38 +40,48 @@ class McategoryController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate(
+        $validator = Validator::make(
+            $request->all(),
             [
-                'category_name'  => 'required|unique:mcategories|max:255',
-                'description'    => 'required|string|min:10|max:255',
-                'photo_name'     => 'required|mimes:jpeg,png,jpg',
-                'brands'         => 'required|array'
+                'category_name_ar'  => ['required|unique:mcategories|max:255'],
+                'category_name_en'  => ['required|unique:mcategories|max:255'],
+                'description_ar'    => ['required|string|min:10|max:255'],
+                'description_en'    => ['required|string|min:10|max:255'],
+                'photo_name'        => ['required|mimes:jpeg,png,jpg'],
+                'brands'            => ['required|array']
             ],
             [
-                'category_name.required'    => 'Please enter the Main Category Name',
-                'category_name.unique'      => 'Main Category Name is already registered',
-                'description.required'      => 'Please enter the Main Category Description',
-                'photo_name.mimes'          => 'Error, Logo must be in one of the required formats',
+                'category_name_ar.required'    => 'Please enter the Main Category Name',
+                'category_name_ar.unique'      => 'Main Category Name is already registered',
+                'description_ar.required'      => 'Please enter the Main Category Description',
+                'photo_name.mimes'             => 'Error, Logo must be in one of the required formats',
             ]
         );
 
         $image = $request->file('photo_name');
         $file_name = $image->getClientOriginalName();
 
-        $mcategory = Mcategory::create([
-            'category_name' => $request->category_name,
-            'description'   => $request->description,
-            'photo_name'    => $file_name
-        ]);
+        $data = [
+            'photo_name'          => $file_name,
+            'ar' => [
+                'category_name'   => $request['category_name_ar'],
+                'description'     => $request['description_ar'],
+            ],
+            'en' => [
+                'category_name'   => $request['category_name_en'],
+                'description'     => $request['description_en'],
+            ],
+        ];
+        $mcategory = Mcategory::create($data);
 
         $mcategory->brands()->attach($request->brands);
 
         // move logo
         // اسم المرفق سيتم حفظه في الداتابيز و لكن المرفق بحد ذاته سيتم حفظه على السيرفر في المكان الذي سنقوم بتحديده
         $imageName = $request->photo_name->getClientOriginalName();
-        $request->photo_name->move(public_path('MainCategoriesLogos/' . $request->category_name), $imageName);
+        $request->photo_name->move(public_path('MainCategoriesLogos/' . $request['category_name_en']), $imageName);
 
-        session()->flash('Add', 'Main Category added successfully');
+        session()->flash('Add', 'تم إضافة التصنيف الرئيسي بنجاح');
         return redirect('/mcategories');
     }
 
@@ -106,12 +117,15 @@ class McategoryController extends Controller
     public function update(Request $request, $id)
     {
         $id = $request->id;
-        $request->validate(
+        $validator = Validator::make(
+            $request->all(),
             [
-                'category_name'  => 'required|max:255|unique:mcategories,category_name,' . $id,
-                'description'    => 'required|string|min:10|max:255',
-                'photo_name'     => 'required|mimes:jpeg,png,jpg',
-                'brands'         => 'required|array'
+                'category_name_ar' => ['required|max:255|unique:mcategories,category_name,' . $id],
+                'category_name_en' => ['required|max:255|unique:mcategories,category_name,' . $id],
+                'description_ar'   => ['required|string|min:10|max:255'],
+                'description_en'   => ['required|string|min:10|max:255'],
+                'photo_name'       => ['required|mimes:jpeg,png,jpg'],
+                'brands'           => ['required|array']
             ],
             [
                 'category_name.required'    => 'Please enter the Main Category Name',
@@ -124,22 +138,29 @@ class McategoryController extends Controller
         $image = $request->file('photo_name');
         $file_name = $image->getClientOriginalName();
 
+        $data = [
+            'photo_name'       => $file_name,
+            'ar' => [
+                'category_name' => $request['category_name_ar'],
+                'description'  => $request['description_ar'],
+            ],
+            'en' => [
+                'category_name' => $request['category_name_en'],
+                'description'  => $request['description_en'],
+            ],
+        ];
         $mcategory = Mcategory::find($id);
 
-        $mcategory->update([
-            'category_name' => $request->category_name,
-            'description'   => $request->description,
-            'photo_name'    => $file_name
-        ]);
+        $mcategory->update($data);
 
         $mcategory->brands()->sync($request->brands);
 
         // move logo
         // اسم المرفق سيتم حفظه في الداتابيز و لكن المرفق بحد ذاته سيتم حفظه على السيرفر في المكان الذي سنقوم بتحديده
         $imageName = $request->photo_name->getClientOriginalName();
-        $request->photo_name->move(public_path('MainCategoriesLogos/' . $request->category_name), $imageName);
+        $request->photo_name->move(public_path('MainCategoriesLogos/' . $request['category_name_en']), $imageName);
 
-        session()->flash('edit', 'Main Category has been successfully modified');
+        session()->flash('edit', 'تم تعديل التصنيف الرئيسي بنجاح');
         return redirect('/mcategories');
     }
 
@@ -153,7 +174,7 @@ class McategoryController extends Controller
     {
         $id = $request->id;
         Mcategory::find($id)->delete();
-        session()->flash('delete', 'Main category has been removed successfully');
+        session()->flash('delete', 'تم حذف التصنيف الرئيسي بنجاح');
         return redirect('/mcategories');
     }
 }
