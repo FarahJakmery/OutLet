1aa9aa85fd422069c8f62d130fb861fa2fe9fc9d
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-03-14 20:15:48 +0200

    Create the register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,81 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.master2')
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+@section('title')
+    تسجيل مستخدم جديد - OutLet
+@endsection
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+@section('css')
+@endsection
 
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
-
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+@section('content')
+    <div class="container-fluid">
+        <div class="row no-gutter">
+            <!-- The content half -->
+            <div class="col-md-6 col-lg-6 col-xl-5 bg-white">
+                <div class="login d-flex align-items-center py-2">
+                    <!-- Demo content-->
+                    <div class="container p-0">
+                        <div class="row">
+                            <div class="col-md-10 col-lg-10 col-xl-9 m-auto">
+                                <div class="card-sigin">
+                                    <div class="mb-5 d-flex">
+                                        {{-- Logo --}}
+                                        <a href="index.html">
+                                            <img src="{{ asset('images/Web_images/Logo.png') }}"
+                                                class="sign-favicon-a ht-40" alt="logo">
+                                            <img src="{{ asset('images/Web_images/Logo.png') }}"
+                                                class="sign-favicon-b ht-40" alt="logo">
+                                        </a>
+                                        <h1 class="main-logo1 ms-1 me-0 my-auto tx-28 ps-1">Out<span>Le</span>t</h1>
+                                    </div>
+                                    <div class="main-signup-header">
+                                        <h2 class="text-orange">مرحبا بك!</h2>
+                                        <h5 class="fw-normal mb-4">الاشراك مجاني و لا يستفرق أكثر من دقيقة.</h5>
+                                        <form method="POST" action="{{ route('register') }}">
+                                            @csrf
+                                            <div class="form-group">
+                                                <label>اسم المستخدم</label>
+                                                <input class="form-control" placeholder="ادخل اسم المستخدم" type="text"
+                                                    name="name">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>البريد الإلكتروني</label>
+                                                <input class="form-control" placeholder="ادخل بريدك الإلكتروني"
+                                                    type="text" name="email">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>كلمة المرور</label> <input class="form-control"
+                                                    placeholder="Enter your password" type="password" name="password">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>تأكيد كلمة المرور</label> <input class="form-control"
+                                                    placeholder="Enter your password" type="password"
+                                                    name="password_confirmation">
+                                            </div>
+                                            <button class="btn btn-warning btn-block">انشأ حساباً</button>
+                                        </form>
+                                        <div class="main-signup-footer mt-5">
+                                            <p>هل لديك حساب سابقاً؟<a href="{{ route('login') }}"> سجل الدخول</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- End -->
+                </div>
+            </div><!-- End -->
+            <!-- The image half -->
+            <div class="col-md-6 col-lg-6 col-xl-7 d-none d-md-flex bg-primary-transparent">
+                <div class="row wd-100p m-auto text-center">
+                    <div class="col-md-12 col-lg-12 col-xl-12 my-auto m-auto wd-100p">
+                        <img src="{{ asset('/images/Web_images/login&register.png') }}" alt="logo">
+                        {{-- <img src="{{ asset('/images/Web_images/login&register.png') }}"
+                            class="my-auto ht-xl-80p wd-md-100p wd-xl-80p m-auto" alt="logo"> --}}
+                    </div>
+                </div>
             </div>
-
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+        </div>
+    </div>
+@endsection
+@section('js')
+@endsection
