0c67e28daceecca16d01b2828db407e87f6ea6d2
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-03-14 20:16:26 +0200

    Create the login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,116 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.master2')
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+@section('title')
+    تسجيل الدخول - OutLet
+@endsection
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+@section('css')
+    <!-- Sidemenu-respoansive-tabs css -->
+    <link href="{{ URL::asset('assets/plugins/sidemenu-responsive-tabs/sidemenu-responsive-tabs.css') }}"
+        rel="stylesheet">
+@endsection
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+@section('content')
+    <div class="container-fluid">
+        <div class="row no-gutter">
+            <!-- The content half -->
+            <div class="col-md-6 col-lg-6 col-xl-5 bg-white">
+                <div class="login d-flex align-items-center py-2">
+                    <!-- Demo content-->
+                    <div class="container p-0">
+                        <div class="row">
+                            <div class="col-md-10 col-lg-10 col-xl-9 mr-center">
+                                <div class="card-sigin">
+                                    <div class="card-sigin">
+                                        <div class="card-sigin d-flex mb-5">
+                                            <a href="index.html">
+                                                {{-- Logo --}}
+                                                <img src="{{ asset('images/Web_images/Logo.png') }}"
+                                                    class="sign-favicon-a ht-40" alt="logo">
+                                                <img src="{{ asset('images/Web_images/Logo.png') }}"
+                                                    class="sign-favicon-b ht-40" alt="logo">
+                                            </a>
+                                            <h1 class="main-logo1 ms-1 me-0 my-auto tx-28 ps-1">Out<span>Le</span>t</h1>
+                                        </div>
+                                        <div class="card-sigin">
+                                            <div class="main-signup-header">
+                                                <h2 class="text-orange">مرحبا بك!</h2>
+                                                <h5 class="fw-semibold mb-4">الرجاء تسجيل الدخول للمتابعة.</h5>
+                                                <form method="POST" action="{{ route('login') }}">
+                                                    @csrf
+                                                    <div class="form-group">
+                                                        <label>البريد الإلكتروني</label>
+                                                        <input id="email" type="email" name="email"
+                                                            class="form-control @error('email') is-invalid @enderror"
+                                                            placeholder="Enter your email" value="{{ old('email') }}"
+                                                            required autocomplete="email" autofocus>
+                                                        @error('email')
+                                                            <span class="invalid-feedback" role="alert">
+                                                                <strong>{{ $message }}</strong>
+                                                            </span>
+                                                        @enderror
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label>كلمة المرور</label>
+                                                        <input id="password" type="password"
+                                                            class="form-control @error('password') is-invalid @enderror"
+                                                            name="password" required autocomplete="current-password"
+                                                            placeholder="Enter your password">
+                                                        @error('password')
+                                                            <span class="invalid-feedback" role="alert">
+                                                                <strong>{{ $message }}</strong>
+                                                            </span>
+                                                        @enderror
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <div class="col-md-6 offset-md-0">
+                                                            <div class="form-check">
+                                                                <input class="form-check-input" type="checkbox"
+                                                                    name="remember" id="remember"
+                                                                    {{ old('remember') ? 'checked' : '' }}>
+                                                                <label class="form-check-label"
+                                                                    for="remember">تذكرني</label>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <button type="submit" class="btn btn-warning btn-block">
+                                                        سجل الدخول
+                                                    </button>
+                                                </form>
+                                                <div class="main-signin-footer mt-5">
+                                                    <p>
+                                                        <a href="">
+                                                            هل نسيت كلمة المرور؟
+                                                        </a>
+                                                    </p>
+                                                    <p>
+                                                        ليس لديك حساب؟
+                                                        <a href="{{ route('register') }}">
+                                                            إنشاء حساب
+                                                        </a>
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- End -->
+                    </div>
+                </div><!-- End -->
             </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+            <!-- The image half -->
+            <div class="col-md-6 col-lg-6 col-xl-7 d-none d-md-flex bg-primary-transparent">
+                <div class="row wd-100p mr-center text-center">
+                    <div class="col-md-12 col-lg-12 col-xl-12 my-auto mr-center wd-100p">
+                        <img src="{{ asset('/images/Web_images/login&register.png') }}" alt="logo">
+                    </div>
+                </div>
             </div>
+        </div>
+    </div>
+@endsection
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+@section('js')
+@endsection
