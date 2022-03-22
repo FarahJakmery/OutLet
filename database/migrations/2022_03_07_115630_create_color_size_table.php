ca04ba43fca3a5627667061006a4d0bd69f6d0ef
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-03-22 08:53:49 +0200

    Create migration for the color_size table

---
+++ database/migrations/2022_03_07_115630_create_color_size_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateColorSizeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('color_size', function (Blueprint $table) {
+            $table->foreignId('color_id')->constrained()->onDelete('cascade');
+            $table->foreignId('size_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('color_size');
+    }
+}
