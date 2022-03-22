e2a3fbffcea2429b79b00f73a3898ce51fd5d409
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-03-22 08:30:42 +0200

    Add Wishlist Routes

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,12 @@ use App\Http\Controllers\Admin\BrandController;
 use App\Http\Controllers\Admin\ColorController;
 use App\Http\Controllers\Admin\McategoryController;
 use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\ProductImageController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\SizeController;
 use App\Http\Controllers\Admin\SubcategoryController;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\WishlistController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -31,19 +33,23 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 
-// Route::group(['middleware' => ['auth']], function () {
-Route::resource('users', UserController::class);
-Route::resource('roles', RoleController::class);
-Route::resource('brands', BrandController::class);
-Route::resource('mcategories', McategoryController::class);
-Route::resource('subcategories', SubcategoryController::class);
-Route::resource('branches', BranchController::class);
-Route::resource('products', ProductController::class);
-Route::resource('sizes', SizeController::class);
-Route::resource('colors', ColorController::class);
-Route::get('/mcategory/{id}', [ProductController::class, 'getSubCategories']);
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', UserController::class);
+    Route::resource('roles', RoleController::class);
+    Route::resource('brands', BrandController::class);
+    Route::resource('mcategories', McategoryController::class);
+    Route::resource('subcategories', SubcategoryController::class);
+    Route::resource('branches', BranchController::class);
+    Route::resource('products', ProductController::class);
+    Route::resource('sizes', SizeController::class);
+    Route::resource('colors', ColorController::class);
+    Route::resource('productImages', ProductImageController::class);
+    Route::get('/maincategory/{id}', [ProductController::class, 'getSubCategories']);
 
-
-// });
+    // WhishList Routes
+    Route::post('wishlist', [WishlistController::class, 'store']);
+    Route::delete('wishlistdestroy', [WishlistController::class, 'destroy'])->name('wishlist.destroy');
+    Route::get('wishlist/products', [WishlistController::class, 'index']);
+});
 
 require __DIR__ . '/auth.php';
