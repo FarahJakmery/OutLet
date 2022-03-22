d20bd094cde509125f62bb7c961a652b98e4dad6
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-03-22 08:49:03 +0200

    Create the Sizes View

---
+++ resources/views/Admin/Sizes/sizes.blade.php
@@ -0,0 +1,297 @@
+@extends('layouts.master')
+
+@section('css')
+    <!-- Internal Data table css -->
+    <link href="{{ URL::asset('assets/plugins/datatable/datatables.min.css') }}" rel="stylesheet" />
+    <link href="{{ URL::asset('assets/plugins/datatable/responsive.dataTables.min.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('assets/plugins/datatable/responsive.bootstrap5.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('assets/plugins/datatable/css/buttons.bootstrap5.min.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('assets/plugins/datatable/css/dataTables.bootstrap5.min.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('assets/plugins/datatable/css/jquery.dataTables.min.css') }}" rel="stylesheet">
+
+    <!--- Animations css-->
+    <link href="{{ URL::asset('assets/css/animate.css') }}" rel="stylesheet">
+
+    <!---Internal Owl Carousel css-->
+    <link href="{{ URL::asset('assets/plugins/owl-carousel/owl.carousel.css') }}" rel="stylesheet">
+
+    <!---Internal  Multislider css-->
+    <link href="{{ URL::asset('assets/plugins/multislider/multislider.css') }}" rel="stylesheet">
+
+    <!--- Select2 css --->
+    <link href="{{ URL::asset('assets/plugins/select2/css/select2.min.css') }}" rel="stylesheet">
+
+    <!---Internal  Prism css-->
+    <link href="{{ URL::asset('assets/plugins/prism/prism.css') }}" rel="stylesheet">
+@endsection
+
+@section('title')
+    المقاسات
+@endsection
+
+@section('page-header')
+    <!-- breadcrumb -->
+    <div class="breadcrumb-header justify-content-between">
+        <div class="my-auto">
+            <div class="d-flex">
+                <h4 class="content-title mb-0 my-auto">المتجر</h4><span class="text-muted mt-1 tx-13 ms-2 mb-0">/
+                    المقاسات </span>
+            </div>
+        </div>
+    </div>
+    <!-- breadcrumb -->
+@endsection
+
+@section('content')
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    @if (session()->has('Add'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>{{ session()->get('Add') }}</strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+
+    @if (session()->has('edit'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>{{ session()->get('edit') }}</strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+
+    @if (session()->has('delete'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>{{ session()->get('delete') }}</strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+
+    <!-- row -->
+    <div class="row row-sm">
+
+        <!--Bordered Table-->
+        <div class="col-xl-12">
+            <div class="card mg-b-20">
+                {{-- card-header --}}
+                <div class="card-header pb-0">
+                    {{-- Add size Button --}}
+                    <div class="col-sm-6 col-md-4 col-xl-3 mg-t-20">
+                        <a class="modal-effect btn btn-outline-primary btn-block" data-bs-effect="effect-flip-vertical"
+                            data-bs-toggle="modal" href="#modaldemo8">إضافة مقاس</a>
+                    </div>
+                </div>
+
+                {{-- card-body --}}
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table id="example" class="table key-buttons text-md-nowrap">
+                            <thead>
+                                <tr>
+                                    <th class="border-bottom-0">#</th>
+                                    <th class="border-bottom-0">اسم المقاس</th>
+                                    <th class="border-bottom-0">الخيارات</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($sizes as $size)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $size->size_name }}</td>
+                                        <td>
+                                            <div class="btn-icon-list">
+                                                {{-- The Edit Button --}}
+                                                <a class="modal-effect btn btn-info btn-icon"
+                                                    data-bs-effect="effect-newspaper" data-id="{{ $size->id }}"
+                                                    data-size_name="{{ $size->size_name }}" data-bs-toggle="modal"
+                                                    href="#EditModal" title="تعديل">
+                                                    <i class="fa fa-edit"></i>
+                                                </a>
+                                                {{-- The Delete Button --}}
+                                                <a class="modal-effect btn btn-danger btn-icon"
+                                                    data-bs-effect="effect-flip-vertical" data-id="{{ $size->id }}"
+                                                    data-size_name="{{ $size->size_name }}" data-bs-toggle="modal"
+                                                    href="#DeleteModal" title="حذف">
+                                                    <i class="fa fa-trash"></i>
+                                                </a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--/div-->
+
+        <!-- Add Section -->
+        <div class="modal fade" id="modaldemo8">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content modal-content-demo">
+                    <div class="modal-header">
+                        <h6 class="modal-title">إضافة مقاس</h6>
+                        <button aria-label="Close" class="close" data-bs-dismiss="modal" type="button">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form action="{{ route('sizes.store') }}" method="POST" autocomplete="on">
+                        {{ csrf_field() }}
+                        <div class="modal-body">
+                            {{-- حقل إدخال اسم المقاس --}}
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">
+                                    <b>اسم المقاس</b>
+                                </label>
+                                <input type="text" class="form-control" name="size_name" id="size_name" required>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-success">تأكيد</button>
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">إغلاق</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- End modal -->
+
+        <!-- Edit Section -->
+        <div class="modal fade" id="EditModal">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content modal-content-demo">
+                    <div class="modal-header">
+                        <h6 class="modal-title">تعديل المقاس</h6><button aria-label="Close" class="close"
+                            data-bs-dismiss="modal" type="button"><span aria-hidden="true">&times;</span></button>
+                    </div>
+                    <form action="sizes/update" method="POST" autocomplete="on">
+                        {{ method_field('patch') }}
+                        {{ csrf_field() }}
+                        <div class="modal-body">
+                            {{-- حقل تعديل اسم المقاس --}}
+                            <div class="form-group">
+                                <input type="hidden" name="id" id="id" value="">
+                                <label for="exampleInputEmail1">
+                                    <b>اسم المقاس</b>
+                                </label>
+                                <input type="text" class="form-control" name="size_name" id="size_name" required>
+                            </div>
+                        </div>
+                        {{-- Footer --}}
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-success">تأكيد</button>
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">إغلاق</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- End modal -->
+
+        <!-- Delete size -->
+        <div class="modal fade" id="DeleteModal">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content tx-size-sm">
+                    <form method="POST" action="sizes/destroy">
+                        {{ method_field('delete') }}
+                        {{ csrf_field() }}
+                        <div class="modal-body tx-center pd-y-20 pd-x-20">
+                            <button aria-label="Close" class="close" data-bs-dismiss="modal" type="button">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            <i class="icon icon ion-ios-close-circle-outline tx-100 tx-danger lh-1 mg-t-20 d-inline-block">
+                            </i>
+                            <h1 class="tx-danger mg-b-20">خطر !!</h1>
+                            <p class="mg-b-20 mg-x-20">
+                            <h3>هل تريد حقا حذف هذا المقاس؟؟</h3>
+                            </p>
+                            <input type="hidden" name="id" id="id" value="">
+                            <input class="form-control" name="size_name" id="size_name" type="text" readonly>
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="submit" class="btn ripple btn-danger">حفظ التغييرات</button>
+                            <button type="button" class="btn ripple btn-secondary" data-bs-dismiss="modal">إغلاق</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Delete size -->
+    </div>
+    <!-- row closed -->
+
+@endsection
+
+@section('js')
+    <!--Internal Data tables -->
+    <script src="{{ URL::asset('assets/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/datatables.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/js/dataTables.bootstrap5.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/js/buttons.bootstrap5.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/js/jszip.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/js/buttons.print.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/js/buttons.colVis.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/pdfmake/vfs_fonts.js') }}"></script>
+    <!--Internal  Datatable js -->
+    <script src="{{ URL::asset('assets/js/table-data.js') }}"></script>
+
+    <!--Internal  Datepicker js -->
+    <script src="{{ URL::asset('assets/plugins/jquery-ui/ui/widgets/datepicker.js') }}"></script>
+
+    <!-- Internal Select2.min js -->
+    <script src="{{ URL::asset('assets/plugins/select2/js/select2.min.js') }}"></script>
+
+    <!-- Internal Modal js-->
+    <script src="{{ URL::asset('assets/js/modal.js') }}"></script>
+
+    <!--Internal  Clipboard js-->
+    <script src="{{ URL::asset('assets/plugins/clipboard/clipboard.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/clipboard/clipboard.js') }}"></script>
+
+    <!-- Internal Prism js-->
+    <script src="{{ URL::asset('assets/plugins/prism/prism.js') }}"></script>
+
+    {{-- This script return the value of each input for editing it --}}
+    <script>
+        $('#EditModal').on('show.bs.modal', function(event) {
+            var button = $(event.relatedTarget)
+            var id = button.data('id')
+            var size_name = button.data('size_name')
+            var modal = $(this)
+            modal.find('.modal-body #id').val(id);
+            modal.find('.modal-body #size_name').val(size_name);
+        })
+    </script>
+
+    {{-- This script return the value of each input for deleting it --}}
+    <script>
+        $('#DeleteModal').on('show.bs.modal', function(event) {
+            var button = $(event.relatedTarget)
+            var id = button.data('id')
+            var size_name = button.data('size_name')
+            var modal = $(this)
+            modal.find('.modal-body #id').val(id);
+            modal.find('.modal-body #size_name').val(size_name);
+        })
+    </script>
+@endsection
