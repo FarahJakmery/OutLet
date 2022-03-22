5111e2f0de2b815c60d9426fcd330b2ed012c816
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-03-22 09:06:41 +0200

    Create a Trait for saving the Image

---
+++ app/Http/Traits/SaveImageTrait.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Traits;
+
+use Intervention\Image\ImageManagerStatic as Image;
+
+trait SaveImageTrait
+{
+    public function saveImage($photo, $folder)
+    {
+        // Saving The Image
+        $imageExtension = $photo->getClientOriginalExtension();
+        $image_name = time() . '.' . $imageExtension;
+        Image::make($photo)->save($folder . '/' . $image_name, 60);
+        return $image_name;
+    }
+}
