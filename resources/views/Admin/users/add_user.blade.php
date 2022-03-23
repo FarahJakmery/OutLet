5870c40eb15f4b6ed5ce85f134e42ab4611de483
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-03-23 08:21:42 +0200

    Create Add user View

--- resources/views/Admin/users/add_user.blade.php
+++ resources/views/Admin/users/add_user.blade.php
@@ -1,22 +1,23 @@
 @extends('layouts.master')
 
-@section('title')
-    Add User
-@endsection
-
 @section('css')
-    <!-- Internal Nice-select css  -->
-    <link href="{{ URL::asset('assets/plugins/jquery-nice-select/css/nice-select.css') }}" rel="stylesheet" />
+    <!--- Animations css-->
+    <link href="../../assets/css/animate.css" rel="stylesheet">
+    <!-- Internal Select2 css -->
+    <link href="../../assets/plugins/select2/css/select2.min.css" rel="stylesheet">
 @endsection
 
+@section('title')
+    إضافة مستخدم
+@endsection
 
 @section('page-header')
     <!-- breadcrumb -->
     <div class="breadcrumb-header justify-content-between">
         <div class="my-auto">
             <div class="d-flex">
-                <h4 class="content-title mb-0 my-auto">Users</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ Add
-                    user</span>
+                <h4 class="content-title mb-0 my-auto">المستخدمين</h4><span class="text-muted mt-1 tx-13 ms-2 mb-0">/
+                    إضافة مستخدم</span>
             </div>
         </div>
     </div>
@@ -26,108 +27,94 @@
 @section('content')
     <!-- row -->
     <div class="row">
-
-
         <div class="col-lg-12 col-md-12">
-
-            @if (count($errors) > 0)
-                <div class="alert alert-danger">
-                    <button aria-label="Close" class="close" data-dismiss="alert" type="button">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    <strong>خطا</strong>
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-
             <div class="card">
                 <div class="card-body">
-                    <div class="col-lg-12 margin-tb">
-                        <div class="pull-right">
-                            <a class="btn btn-primary btn-sm" href="{{ route('users.index') }}">رجوع</a>
-                        </div>
-                    </div><br>
-                    <form class="parsley-style-1" id="selectForm2" autocomplete="off" name="selectForm2"
-                        action="{{ route('users.store', 'test') }}" method="post">
+                    <form action="{{ route('users.store') }}" method="POST">
                         {{ csrf_field() }}
-
-                        <div class="">
-
-                            <div class="row mg-b-20">
-                                <div class="parsley-input col-md-6" id="fnWrapper">
-                                    <label>اسم المستخدم: <span class="tx-danger">*</span></label>
-                                    <input class="form-control form-control-sm mg-b-20"
-                                        data-parsley-class-handler="#lnWrapper" name="name" required="" type="text">
-                                </div>
-
-                                <div class="parsley-input col-md-6 mg-t-20 mg-md-t-0" id="lnWrapper">
-                                    <label>البريد الالكتروني: <span class="tx-danger">*</span></label>
-                                    <input class="form-control form-control-sm mg-b-20"
-                                        data-parsley-class-handler="#lnWrapper" name="email" required="" type="email">
+                        <h3>المعلومات الشخصية</h3>
+                        <div class="row">
+                            <div class="col">
+                                <div class="control-group form-group">
+                                    <label class="form-label">اسم المستخدم:<span class="tx-danger">*</span></label>
+                                    <input type="text" class="form-control required" placeholder="اسم المستخدم" name="name"
+                                        data-bs-placement="bottom" data-bs-toggle="tooltip" title="يرجى ادخال اسم المستخدم"
+                                        required="">
                                 </div>
                             </div>
-
-                        </div>
-
-                        <div class="row mg-b-20">
-                            <div class="parsley-input col-md-6 mg-t-20 mg-md-t-0" id="lnWrapper">
-                                <label>كلمة المرور: <span class="tx-danger">*</span></label>
-                                <input class="form-control form-control-sm mg-b-20" data-parsley-class-handler="#lnWrapper"
-                                    name="password" required="" type="password">
-                            </div>
-
-                            <div class="parsley-input col-md-6 mg-t-20 mg-md-t-0" id="lnWrapper">
-                                <label> تاكيد كلمة المرور: <span class="tx-danger">*</span></label>
-                                <input class="form-control form-control-sm mg-b-20" data-parsley-class-handler="#lnWrapper"
-                                    name="confirm-password" required="" type="password">
+                            <div class="col">
+                                <div class="control-group form-group">
+                                    <label class="form-label">البريد الإلكتروني:<span
+                                            class="tx-danger">*</span></label>
+                                    <input type="email" class="form-control required" placeholder="example@gmail.com"
+                                        name="email" data-bs-placement="bottom" data-bs-toggle="tooltip"
+                                        title="يرجى ادخال البريد الإلكتروني" required="">
+                                </div>
                             </div>
                         </div>
+                        <div class="row">
+                            <div class="col">
+                                <div class="control-group form-group">
+                                    <label class="form-label">كلمة المرور:<span class="tx-danger">*</span></label>
+                                    <input type="password" class="form-control required" placeholder="********"
+                                        name="password" data-bs-placement="bottom" data-bs-toggle="tooltip"
+                                        title="يرجى ادخال كلمة المرور" required="">
+                                </div>
+                            </div>
+                            <div class="col">
 
-                        <div class="row row-sm mg-b-20">
-                            <div class="col-lg-6">
-                                <label class="form-label">حالة المستخدم</label>
-                                <select name="Status" id="select-beast" class="form-control  nice-select  custom-select">
-                                    <option value="مفعل">مفعل</option>
-                                    <option value="غير مفعل">غير مفعل</option>
+                                <div class="control-group form-group mb-0">
+                                    <label class="form-label">تأكيد كلمة المرور:<span
+                                            class="tx-danger">*</span></label>
+                                    <input type="password" class="form-control required" placeholder="********"
+                                        name="confirm-password" data-bs-placement="bottom" data-bs-toggle="tooltip"
+                                        title="يرجى تأكيد كلمة المرور" required="">
+                                </div>
+                            </div>
+                        </div>
+                        <h3>حالة المستخدم و الأدوار</h3>
+                        <div class="row">
+                            <div class="col">
+                                <label class="form-label">حالة المستخدم:<span class="tx-danger">*</span></label>
+                                <select class="form-control select2" required="" name="Status">
+                                    <option label="Choose one">
+                                    </option>
+                                    <option value="مفعل">
+                                        مفعل
+                                    </option>
+                                    <option value="غير مفعل">
+                                        غير مفعل
+                                    </option>
+
                                 </select>
                             </div>
-                        </div>
+                            <div class="col">
+                                <div class="col-lg-4 mg-b-20 mg-lg-b-0">
+                                    <p class="mg-b-10">أدوار المستخدم</p>
+                                    {!! Form::select('roles_name[]', $roles, [], ['class' => 'form-control', 'multiple']) !!}
 
-                        <div class="row mg-b-20">
-                            <div class="col-xs-12 col-md-12">
-                                <div class="form-group">
-                                    <label class="form-label"> صلاحية المستخدم</label>
-                                    {{-- {!! Form::select('roles_name[]', $roles, [], ['class' => 'form-control', 'multiple']) !!} --}}
                                 </div>
                             </div>
                         </div>
-                        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                            <button class="btn btn-main-primary pd-x-20" type="submit">تاكيد</button>
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                                <button class="btn btn-main-primary pd-x-20" type="submit">تأكيد</button>
+                            </div>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
+    </div>
     <!-- row closed -->
-    </div>
-    <!-- Container closed -->
-    </div>
-    <!-- main-content closed -->
 @endsection
 
-@section('scripts')
-    <!-- Internal Nice-select js-->
-    <script src="{{ URL::asset('assets/plugins/jquery-nice-select/js/jquery.nice-select.js') }}"></script>
-    <script src="{{ URL::asset('assets/plugins/jquery-nice-select/js/nice-select.js') }}"></script>
-
-    <!--Internal  Parsley.min js -->
-    <script src="{{ URL::asset('assets/plugins/parsleyjs/parsley.min.js') }}"></script>
-
-    <!-- Internal Form-validation js -->
-    <script src="{{ URL::asset('assets/js/form-validation.js') }}"></script>
+@section('js')
+    <!--Internal  Select2 js -->
+    <script src="../../assets/plugins/select2/js/select2.min.js"></script>
+    <!-- JQuery min js -->
+    <script src="../../assets/plugins/jquery/jquery.min.js"></script>
+    <!-- Internal form-elements js -->
+    <script src="../../assets/js/form-elements.js"></script>
 @endsection
