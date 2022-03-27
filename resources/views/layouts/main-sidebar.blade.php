3a68e62ff34e99ea2f0825974e975b69aad5fb4e
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-03-27 18:00:35 +0300

    Add a "side-menu-item" to the "main-sidebar" layout

--- resources/views/layouts/main-sidebar.blade.php
+++ resources/views/layouts/main-sidebar.blade.php
@@ -158,6 +158,21 @@
                     </ul>
                 </li>
             @endcan
+
+            <li class="slide">
+                <a class="side-menu__item" data-bs-toggle="slide" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24">
+                        <path d="M0 0h24v24H0V0z" fill="none" />
+                        <path d="M13 4H6v16h12V9h-5V4zm3 14H8v-2h8v2zm0-6v2H8v-2h8z" opacity=".3" />
+                        <path
+                            d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z" />
+                    </svg><span class="side-menu__label">الطلبات</span><i class="angle fe fe-chevron-down"></i></a>
+                <ul class="slide-menu">
+                    <li><a class="slide-item" href="{{ url('/' . ($page = 'orders')) }}">قائمة الطلبات</a></li>
+                    <li><a class="slide-item" href="{{ url('/' . ($page = 'statuses')) }}">حالات الطلب</a></li>
+                </ul>
+            </li>
+
             {{-- <li class="side-item side-item-category">General</li>
             <li class="slide">
                 <a class="side-menu__item" href="icons.html">
