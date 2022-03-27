d2b24cd1cdbbe5a0ce140d1450c42405f946a9e8
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-03-27 18:23:29 +0300

    Create Order Controller & implement (index,show,destroy) Methodes

---
+++ app/Http/Controllers/Admin/OrderController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\Status;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $orders = Order::all();
+        $statuses = Status::all();
+        return view('Admin.Orders.orders', compact('orders', 'statuses'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $order = Order::find($id);
+        $statuses = $order->statuses->pluck('status', 'id')->all();
+        return view('Admin.Orders.orders', compact('order', 'statuses'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Order $order)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Order $order)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $id = $request->id;
+        Order::find($id)->delete();
+        session()->flash('delete', 'تم حذف الطلب بنجاح');
+        return redirect('/orders');
+    }
+}
