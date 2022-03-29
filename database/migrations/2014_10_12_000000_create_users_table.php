2468aed832902fa7884d7676a02f0d41030b8ee8
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-03-29 13:17:01 +0300

    Add some fields to "users" migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->text('roles_name'); // I put the type of this column "text" becuse the user may have more than one role
-            $table->string('Status', 10); // This column will be "Active" or "Non Active"
+            $table->string('mobile_number');
+            $table->text('roles_name')->nullable(); // I put the type of this column "text" becuse the user may have more than one role
+            $table->string('Status', 10)->nullable(); // This column will be "Active" or "Non Active"
             $table->rememberToken();
             $table->timestamps();
         });
