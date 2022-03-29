2f4fdfb83b13059a7d7af17a12ae96513f40cdf6
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-03-29 11:13:31 +0300

    Setup JWT Authentication Package

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable, HasRoles;
 
@@ -18,7 +19,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'roles_name', 'Status'
+        'first_name', 'last_name', 'email', 'password', 'mobile_number', 'roles_name', 'Status'
     ];
 
     /**
@@ -39,6 +40,26 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'roles_name'        => 'array', // I want to store this in the database as array so I changed its type
     ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
 
     /**
      * Get the wishlist of the user.
