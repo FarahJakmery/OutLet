2d835eb6959e6fa517854e7da50c20900860f8e6
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-03-30 22:11:47 +0300

    Add Some Routes To the Web.php file

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\BranchController;
+use App\Http\Controllers\Api\BrandController;
+use App\Http\Controllers\Api\McategoryController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\SubcategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +20,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);
+});
+
+
+Route::group(['middleware' => ['jwt.verify']], function () {
+    Route::resource('Brands', BrandController::class);
+    Route::resource('MainCategories', McategoryController::class);
+    Route::resource('SubCategories', SubcategoryController::class);
+    Route::resource('Branches', BranchController::class);
+    Route::resource('Products', ProductController::class);
 });
