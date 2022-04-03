508cacc82a8581575f05d249fe9779db8830a640
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-04-03 08:58:31 +0300

    Create Api Brand Controller & Implement "Index,Show" Methodes

---
+++ app/Http/Controllers/Api/BrandController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use Illuminate\Http\Request;
+
+class BrandController extends Controller
+{
+    use ApiResponseTrait;
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $Brands = Brand::translated()->get();
+
+        return $this->apiResponse($Brands, 'OK', 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $Brand = Brand::find($id)->with('products.images:id,image_name,product_id')->get();
+        if ($Brand) {
+            return $this->apiResponse($Brand, 'OK', 200);
+        }
+        return $this->apiResponse($Brand, 'The Brand Not Found', 401);
+    }
+}
