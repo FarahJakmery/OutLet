6319aaeb412e71faa320cb28925eb820792c72ce
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-04-06 08:59:40 +0300

    Create Review Controller & Implement "store,destroy" Methodes for Customer

---
+++ app/Http/Controllers/Customer/ReviewController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Customer;
+
+use App\Http\Controllers\Controller;
+use App\Models\Review;
+use Illuminate\Http\Request;
+
+class ReviewController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Review::create($request->all() + ['user_id' => auth()->id()]);
+        return redirect()->route('products.show', $request->product_id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $review = Review::find($request->id);
+        $review->delete();
+        session()->flash('delete', 'تم حذف المراجعة بنجاح');
+        return redirect('/reviews');
+    }
+}
