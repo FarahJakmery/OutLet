7b895b030daefb752f120f0d75e11d3a32a1d076
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-04-06 08:51:19 +0300

    Create Review View for Admin

---
+++ resources/views/Admin/Reviews/reviews.blade.php
@@ -0,0 +1,220 @@
+@extends('layouts.master')
+
+@section('css')
+    <!-- Internal Data table css -->
+    <link href="{{ URL::asset('assets/plugins/datatable/datatables.min.css') }}" rel="stylesheet" />
+    <link href="{{ URL::asset('assets/plugins/datatable/responsive.dataTables.min.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('assets/plugins/datatable/responsive.bootstrap5.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('assets/plugins/datatable/css/buttons.bootstrap5.min.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('assets/plugins/datatable/css/dataTables.bootstrap5.min.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('assets/plugins/datatable/css/jquery.dataTables.min.css') }}" rel="stylesheet">
+
+    <!--- Animations css-->
+    <link href="{{ URL::asset('assets/css/animate.css') }}" rel="stylesheet">
+
+    <!---Internal Owl Carousel css-->
+    <link href="{{ URL::asset('assets/plugins/owl-carousel/owl.carousel.css') }}" rel="stylesheet">
+
+    <!---Internal  Multislider css-->
+    <link href="{{ URL::asset('assets/plugins/multislider/multislider.css') }}" rel="stylesheet">
+
+    <!--- Select2 css --->
+    <link href="{{ URL::asset('assets/plugins/select2/css/select2.min.css') }}" rel="stylesheet">
+
+    <!---Internal  Prism css-->
+    <link href="{{ URL::asset('assets/plugins/prism/prism.css') }}" rel="stylesheet">
+
+    <!--Internal  Font Awesome -->
+    <link href="{{ URL::asset('assets/plugins/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+
+    <!--Internal Sumoselect css-->
+    <link rel="stylesheet" href="{{ URL::asset('assets/plugins/sumoselect/sumoselect-rtl.css') }}">
+@endsection
+
+@section('title')
+    المراجعات
+@endsection
+
+@section('page-header')
+    <!-- breadcrumb -->
+    <div class="breadcrumb-header justify-content-between">
+        <div class="my-auto">
+            <div class="d-flex">
+                <h4 class="content-title mb-0 my-auto">المنتجات</h4><span class="text-muted mt-1 tx-13 ms-2 mb-0">/
+                    المراجعات</span>
+            </div>
+        </div>
+    </div>
+    <!-- breadcrumb -->
+@endsection
+
+@section('content')
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    @if (session()->has('Add'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>{{ session()->get('Add') }}</strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+
+    @if (session()->has('edit'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>{{ session()->get('edit') }}</strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+
+    @if (session()->has('delete'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>{{ session()->get('delete') }}</strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+
+    <!-- row -->
+    <div class="row row-sm">
+
+        <!--Bordered Table-->
+        <div class="col-xl-12">
+            <div class="card mg-b-20">
+                {{-- card-body --}}
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table id="example" class="table key-buttons text-md-nowrap">
+                            <thead>
+                                <tr>
+                                    <th class="border-bottom-0"><b>#</b></th>
+                                    <th class="border-bottom-0"><b>التاريخ والوقت</b></th>
+                                    <th class="border-bottom-0"><b>اسم المنتج</b></th>
+                                    <th class="border-bottom-0"><b>اسم المستخدم</b></th>
+                                    <th class="border-bottom-0"><b>التقييم</b></th>
+                                    <th class="border-bottom-0"><b>المراجعة</b></th>
+                                    <th class="border-bottom-0"><b>الخيارات</b></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {{-- @if (count($reviews > 0)) --}}
+                                @foreach ($reviews as $review)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $review->created_at }}</td>
+                                        <td>{{ $review->product->translate('ar')->product_name }}</td>
+                                        <td>{{ $review->user->first_name }} {{ $review->user->last_name }}</td>
+                                        <td>{{ $review->rating }}</td>
+                                        <td>{{ $review->comment }}</td>
+                                        <td>
+                                            <div class="btn-icon-list">
+                                                {{-- The Delete Button --}}
+                                                <a class="modal-effect btn btn-danger btn-icon"
+                                                    data-bs-effect="effect-flip-vertical" data-id="{{ $review->id }}"
+                                                    data-bs-toggle="modal" href="#DeleteModal" title="حذف">
+                                                    <i class="fa fa-trash"></i>
+                                                </a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                {{-- @endif --}}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--/div-->
+
+        <!-- Delete Review -->
+        <div class="modal fade" id="DeleteModal">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content tx-size-sm">
+                    <form method="POST" action="reviews/destroy">
+                        {{ method_field('delete') }}
+                        {{ csrf_field() }}
+                        <div class="modal-body tx-center pd-y-20 pd-x-20">
+                            <button aria-label="Close" class="close" data-bs-dismiss="modal" type="button">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            <i class="icon icon ion-ios-close-circle-outline tx-100 tx-danger lh-1 mg-t-20 d-inline-block">
+                            </i>
+                            <h1 class="tx-danger mg-b-20">خطر !!</h1>
+                            <p class="mg-b-20 mg-x-20">
+                            <h3>هل تريد حقا حذف هذه المراجعة؟؟</h3>
+                            </p>
+                            <input type="hidden" name="id" id="id" value="">
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="submit" class="btn ripple btn-danger">حفظ التغييرات</button>
+                            <button type="button" class="btn ripple btn-secondary" data-bs-dismiss="modal">إغلاق</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Delete Branch -->
+    </div>
+    <!-- row closed -->
+@endsection
+
+@section('js')
+    <!--Internal Data tables -->
+    <script src="{{ URL::asset('assets/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/datatables.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/js/dataTables.bootstrap5.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/js/buttons.bootstrap5.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/js/jszip.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/js/buttons.print.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/js/buttons.colVis.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/datatable/pdfmake/vfs_fonts.js') }}"></script>
+    <!--Internal  Datatable js -->
+    <script src="{{ URL::asset('assets/js/table-data.js') }}"></script>
+
+    <!--Internal  Form-elements js-->
+    <script src="{{ URL::asset('assets/js/advanced-form-elements.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/select2.js') }}"></script>
+
+    <!-- Internal Select2.min js -->
+    <script src="{{ URL::asset('assets/plugins/select2/js/select2.min.js') }}"></script>
+
+    <!--Internal Sumoselect js-->
+    <script src="{{ URL::asset('assets/plugins/sumoselect/jquery.sumoselect.js') }}"></script>
+
+    <!-- Internal Modal js-->
+    <script src="{{ URL::asset('assets/js/modal.js') }}"></script>
+
+    <!--Internal  Clipboard js-->
+    <script src="{{ URL::asset('assets/plugins/clipboard/clipboard.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/clipboard/clipboard.js') }}"></script>
+
+    <!-- Internal Prism js-->
+    <script src="{{ URL::asset('assets/plugins/prism/prism.js') }}"></script>
+
+    {{-- This script return the value of each input for deleting it --}}
+    <script>
+        $('#DeleteModal').on('show.bs.modal', function(event) {
+            var button = $(event.relatedTarget)
+            var id = button.data('id')
+            var modal = $(this)
+            modal.find('.modal-body #id').val(id);
+        })
+    </script>
+@endsection
