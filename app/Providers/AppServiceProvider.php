548c4f4a1715f0e2f020570a9caed69ee8c2d122
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-06-13 11:33:40 +0300

    Get Data To share it with all views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use App\Http\View\Composers\ProductComposer;
+use App\Models\Brand;
+use App\Models\Mcategory;
+use App\Models\Size;
+use App\Models\Subcategory;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 
@@ -25,7 +29,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Using class based composers...
+        // Sharing Data With All Views
+        View::share('Brands', Brand::all());
+        View::share('mainCategories', Mcategory::all());
+        View::share('SubCategories', Subcategory::all());
+        View::share('Sizes', Size::all());
+        // View Composers
         View::composer('*', ProductComposer::class);
     }
 }
